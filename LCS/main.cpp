6250953aab60ecb651460500fc150564db11e8b4
VictoriaRmrz <A01640824@itesm.mx>
2024-01-10 20:28:53 -0600

    added some explanations

--- LCS/main.cpp
+++ LCS/main.cpp
@@ -7,10 +7,14 @@ int lcs(vector<string> X, vector<string> Y, int m, int n) // X and Y are the inp
 
     if (m == 0 || n == 0)
         return 0;
-    if (X[m-1] == Y[n-1])
-        return 1 + lcs(X, Y, m-1, n-1);
+    if (X[m-1] == Y[n-1]) // checks if the last letter are the same
+        return 1 + lcs(X, Y, m-1, n-1); // sums when a coincidence is found
     else
-        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
+        return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));  // iterates between the last letter of a vector and compare it between
+                                                           // each letter of the other vector, and when there is a coincidence
+                                                           // it sums 1 to a counter, then checks the maximum of coincidences in between 
+                                                           // the two vectors
+
 }
 int main() {
 
